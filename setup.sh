3d0b19c23b8796ec323204ce1cc55233e4c90fa8
Mark Furland <mtfurlan@i3detroit.org>
2018-03-21 11:33:11 -0400

    setup script

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "trying to install things"
+
+if [ -x "$(which apt-get)" ] ; then
+  sudo apt-get install vim-nox tmux git sl silversearcher-ag curl
+else
+  echo "apt-get not installed, fix setup.sh for this platform"
+fi
