95f0b4b3a507f82f2f81d92576c80e55c9c113af
Mark Furland <mtfurlan@i3detroit.org>
2019-09-08 17:29:26 -0400

    commented out command to fix trackpoint sometimes

--- bin/disableTouchpad.sh
+++ bin/disableTouchpad.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 xinput --disable $(xinput | sed -n 's/^.*TouchPad\W*id=\([0-9]*\).*$/\1/p')
+# If mouse stops working on thinkpad, this has helped: https://bugzilla.redhat.com/show_bug.cgi?id=1228566
+# echo -n rescan | sudo tee /sys/devices/platform/i8042/serio1/drvctl
